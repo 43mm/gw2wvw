b1b42ffce3f917e358fe8914dd4d80d71f10fa26
43mm <18689574+43mm@users.noreply.github.com>
2025-03-10 22:00:04 +1100

    chore: add react query

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
+
 function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <div>
         <h1 className="text-5xl font-bold">GW2 WvW</h1>
       </div>
-    </>
+    </QueryClientProvider>
   );
 }
 
