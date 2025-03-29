f621fc86659c8d5e364144faa884ac0d6f71e993
43mm <18689574+43mm@users.noreply.github.com>
2025-03-29 15:12:54 +1100

    feat: add match start and end times

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,62 @@
 import { useQuery } from "@tanstack/react-query";
+import { format, formatDistanceToNow, parseISO } from "date-fns";
 import { fetchAllMatchesOverview } from "./api";
 import { WORLDS } from "./data/worlds";
-import { MatchOverview, TeamCount } from "./schemas";
+import { MatchOverview } from "./schemas";
 
-function selectWorldIds(matches: MatchOverview[]): [TeamCount[], TeamCount[]] {
-  const [na, eu] = [[], []] as [TeamCount[], TeamCount[]];
-  for (const { id, all_worlds: w } of matches) {
-    const team = {
+type Match = {
+  red: number;
+  blue: number;
+  green: number;
+  startTime: Date;
+  endTime: Date;
+};
+
+function selectWorldIds(matches: MatchOverview[]): [Match[], Match[]] {
+  const [na, eu] = [[], []] as [Match[], Match[]];
+  for (const { id, all_worlds: w, start_time, end_time } of matches) {
+    const Match = {
       red: Math.max(...w.red),
       blue: Math.max(...w.blue),
       green: Math.max(...w.green),
+      startTime: parseISO(start_time),
+      endTime: parseISO(end_time),
     };
-    (id.startsWith("1") ? na : eu).push(team);
+    (id.startsWith("1") ? na : eu).push(Match);
   }
   return [na, eu];
 }
 
 function App() {
-  const { data: matches } = useQuery({
+  const { data: matches, isLoading } = useQuery({
     queryKey: ["matches"],
     queryFn: fetchAllMatchesOverview,
     select: selectWorldIds,
   });
 
+  if (isLoading) return <div>Loading...</div>;
+  if (matches === undefined) return <div>Error...</div>;
+
   return (
     <div>
       <h1 className="text-5xl font-bold">GW2 WvW</h1>
       <h5 className="text-xl font-semibold">NA</h5>
+      <div>
+        From: {format(matches[0][0].startTime, "yyyy-MM-dd HH:mm")} (
+        {formatDistanceToNow(matches[0][0].startTime, {
+          addSuffix: true,
+          includeSeconds: true,
+        })}
+        )
+      </div>
+      <div>
+        To: {format(matches[0][0].endTime, "yyyy-MM-dd HH:mm")} (
+        {formatDistanceToNow(matches[0][0].endTime, {
+          addSuffix: true,
+          includeSeconds: true,
+        })}
+        )
+      </div>
       <div className="grid grid-cols-3 gap-2">
         {matches &&
           matches[0].map((match) => (
@@ -38,6 +68,22 @@ function App() {
           ))}
       </div>
       <h5 className="text-xl font-semibold">EU</h5>
+      <div>
+        From: {format(matches[1][0].startTime, "yyyy-MM-dd HH:mm")} (
+        {formatDistanceToNow(matches[0][0].startTime, {
+          addSuffix: true,
+          includeSeconds: true,
+        })}
+        )
+      </div>
+      <div>
+        To: {format(matches[1][0].endTime, "yyyy-MM-dd HH:mm")} (
+        {formatDistanceToNow(matches[0][0].endTime, {
+          addSuffix: true,
+          includeSeconds: true,
+        })}
+        )
+      </div>
       <div className="grid grid-cols-3 gap-2">
         {matches &&
           matches[1].map((match) => (
